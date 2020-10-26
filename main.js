6da791b648de13b19b2878b2802155dd0ab706a6
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-10-26 16:01:08 +0100

    Fade In Fade Out

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-$('.fas.fa-bars').click( function () {
-    $('.hamburger-menu').show();
-})
+// $('.fas.fa-bars').click( function () {
+//     $('.hamburger-menu').show();
+// })
 
-$('.fas.fa-times').click( function () {
-    $('.hamburger-menu').hide();
-})
+// $('.fas.fa-times').click( function () {
+//     $('.hamburger-menu').hide();
+// })
+
+$('.fas.fa-bars').click( function () {
+
+        $('.hamburger-menu').fadeIn();
+    })
+
+    $('.fas.fa-times').click( function () {
+
+        $('.hamburger-menu').fadeOut();
+    })
